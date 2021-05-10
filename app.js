e9a30c2e5f66507c7be2e0c2135329093f7e9bba
unknown <chinwe.promise2016@gmail.com>
2021-05-09 21:30:38 -0700

    Installed dotenv and added cloud db

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
 
-mongoose.connect('mongodb://localhost/CRUDAPP', {useUnifiedTopology: true,  useNewUrlParser: true, useFindAndModify: false })
-    .then(()=> console.log('Connected to database'))
-    .catch((err) => console.log('Connection Failed'))
+dotenv.config({
+  path: "./config.env",
+});
+mongoose
+  .connect(
+    `mongodb+srv://wendy:${process.env.ATLAS_PASSWORD}@crudappdata.eoxtw.mongodb.net/crudApp?retryWrites=true&w=majority`,
+    { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false }
+  )
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.log("Connection Failed"));
 
-const users = require('./routes/users')
+const users = require("./routes/users");
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use('/app/data', users)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/app/data", users);
 
-
-const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=> console.log(`App is running on port ${PORT}`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
